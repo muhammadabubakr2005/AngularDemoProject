677f25911aa35a0bdbf7f99461fd44875607e47a
Muhammad Abubakar <139749406+muhammadabubakr2005@users.noreply.github.com>
2025-06-20 12:32:19 +0500

    added pagination in user-list

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -18,25 +18,45 @@ export class UserListComponent {
   protected currentUser: User | null = null;
   protected users: User[]=[];
   protected searchTerm: string = '';
+  protected currentPage: number = 1;
+  protected pageSize: number = 5;
+  protected paginatedUsers: User[] = []
+  math = Math;
   constructor(
     private userService: UserService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    
   ) {}
   ngOnInit() {
     this.currentUser = this.authService.getUser();
     this.loadUsers();
+    console.log('Paginated users:', this.paginatedUsers);
   }
+  // ngOnChanges() {
+  //   this.loadUsers();
+  // }
   loadUsers() {
     this.userService.getUsers().subscribe({
       next: (users: User[]) => {
         this.users = users;
+        this.paginatedUsers = this.getPaginatedUsers();
         console.log('Users loaded:', this.users);
       },
       error: (err: any) => {
         console.error('Error loading users:', err);
       }
     });
+  }
+
+  getPaginatedUsers(): User[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.users.slice(start, start + this.pageSize);
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.paginatedUsers = this.getPaginatedUsers();
   }
   viewUser(user: User) {
     this.router.navigate(['/view-user', user.id]);
@@ -46,8 +66,14 @@ export class UserListComponent {
     this.userService.updateUser(user.id, user).subscribe({
       next: () => {
         console.log('User deleted:', user.id);
-        this.loadUsers();
-      },
+        // this.loadUsers();
+        this.users = this.users.filter(u => u.id !== user.id);
+        const totalPages = Math.ceil(this.users.length / this.pageSize);
+        if (this.currentPage > totalPages) {
+          this.currentPage = totalPages;
+        }
+        this.paginatedUsers = this.getPaginatedUsers();
+        },
       error: (err: any) => {
         console.error('Error deleting user:', err);
       }
@@ -56,11 +82,13 @@ export class UserListComponent {
   editUser(user:User){
     this.router.navigate(['/edit-user', user.id]);
   }
+  
   onSearch(searchTerm: string) {
     if (searchTerm) {
       this.userService.searchUsers(searchTerm).subscribe({
         next: (users: User[]) => {
           this.users = users;
+          this.paginatedUsers = users;
           console.log('Search results:', this.users);
         },
         error: (err: any) => {
